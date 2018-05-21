65e92244bf32a4d54c9ffb87015466ed49fbb981
giovanni rescaglio <giovannirescaglio@gmail.com>
2018-05-21 18:55:24 +0200

    routes creation + association

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ApplicationRecord
   belongs_to :sport
   belongs_to :state
   belongs_to :user
+  has_many :reviews, through: :bookings
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'articles', to: 'articles#index'
+  post 'articles', to: 'articles#create'
+  get 'articles/new', to: 'articles#new'
+  get 'articles/:id', to: 'articles#show'
+  post 'bookings', to: 'bookings#create'
+  get 'bookings/:id/reviews/new', to: 'reviews#new'
+  post 'reviews', to: 'reviews#create'
+  get 'dashboard/listings', to: 'dashboard#listings'
+  get 'dashboard/rentals', to: 'dashboard#rentals'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

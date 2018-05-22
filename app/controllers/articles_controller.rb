a8b2afe09a3a69a83fa80ad2d0432be8a1b12f89
Laura Martin <laumartin4@gmail.com>
2018-05-22 12:39:07 +0200

    created articles new, create and seeds for sports and states

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,34 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
-  def create
+  def new
+    @sports = Sport.all
+    @states = State.all
+    @article = Article.new
   end
 
-  def new
+  def create
+    @article = Article.new(article_params)
+    @article.user = current_user
+    @article.save!
   end
+
 
   def show
   end
+
+private
+
+  def article_params
+    params.require(:article).permit(:sport_id, :state_id, :name, :description, :start_date, :end_date, :latitude, :longitude, :picture, :price)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+
 end

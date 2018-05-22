5a57320effa306e603b18803f2ef8a2f132d02df
Antoine Goffette <a.goffette@gmail.com>
2018-05-22 13:12:46 +0200

    filtering with a sport params in index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   def index
     @articles = policy_scope(Article)
+    @articles = @articles.where(sport_id: Sport.find_by_name((params[:sport])).id) if params[:sport].present?
   end
 
   def create

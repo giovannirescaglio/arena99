0bb346488b074ece20ae35ebc5c0575e7d600356
giovanni rescaglio <giovannirescaglio@gmail.com>
2018-05-23 16:12:15 +0200

    redirect done

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
-  skip_after_action :verify_authorized, only: :listings
+  skip_after_action :verify_authorized, only: [:listings, :rentals]
 
   def listings
     @articles = current_user.articles

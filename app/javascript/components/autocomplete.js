a1672f91e8564fb859b9c181c95d5b26ff5c7159
Laura Martin <laumartin4@gmail.com>
2018-05-24 15:01:08 +0200

    have done the login/logout as well as the autocomplete on home page for location

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -1,7 +1,6 @@
 function autocomplete() {
   document.addEventListener("DOMContentLoaded", function() {
     var articleAddress = document.getElementById('article_address');
-
     if (articleAddress) {
       var autocomplete = new google.maps.places.Autocomplete(articleAddress, { types: [ 'geocode' ] });
       google.maps.event.addDomListener(articleAddress, 'keydown', function(e) {
@@ -11,6 +10,21 @@ function autocomplete() {
       });
     }
   });
+  document.addEventListener("DOMContentLoaded", function() {
+    var localisation = document.getElementById('localisation');
+    if (localisation) {
+      var autocomplete = new google.maps.places.Autocomplete(localisation, { types: [ 'geocode' ] });
+      google.maps.event.addDomListener(localisation, 'keydown', function(e) {
+        if (e.key === "Enter") {
+          e.preventDefault(); // Do not submit the form on Enter.
+        }
+      });
+    }
+  });
 }
 
+
+
 export { autocomplete };
+
+

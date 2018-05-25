fd3608a27cbce329792e54c51d77574df907a3ad
giovanni rescaglio <giovannirescaglio@gmail.com>
2018-05-25 14:03:31 +0200

    javascript fixes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,11 +5,10 @@ import {activeTab} from '../components/activeTab';
 import "hover.css/css/hover.css";
 import "animate.css/animate.css";
 import "animate.css/animate.min.css";
-import "aos/src/js/aos.js";
-import "aos/dist/aos.css";
+
 
 if (document.getElementsByClassName('nav-tabs')) {
  activeTab();
 };
 
-AOS.init();
+

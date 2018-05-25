11ce2df672864021415d64b8fc152a70a2b8f4a0
Antoine Goffette <a.goffette@gmail.com>
2018-05-25 13:45:33 +0200

    done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,15 @@
 import "bootstrap";
 import "../plugins/flatpickr";
 import "../plugins/booking";
-import {activeTab} from '../components/activeTab'
+import {activeTab} from '../components/activeTab';
+import "hover.css/css/hover.css";
+import "animate.css/animate.css";
+import "animate.css/animate.min.css";
+import "aos/src/js/aos.js";
+import "aos/dist/aos.css";
 
 if (document.getElementsByClassName('nav-tabs')) {
  activeTab();
 };
 
+AOS.init();
